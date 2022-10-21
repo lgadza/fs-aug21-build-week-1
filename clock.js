b1ecbdbc1da5fd31f1bd628a81a5bd7ed978f7fb
Louis Gadza <siuolgadza@gmail.com>
2022-10-21 14:55:18 +0200

    clock

--- clock.js
+++ clock.js
@@ -1,78 +1,103 @@
-timeH = document.getElementById("secNum");
-let timeSecond = 5;
+//The time timer inside the clock
+let btn = document.querySelector(".btn");
+let timeH = document.querySelector("#secNum");
+let timeSecond = 10;
 
-function displayTime(second) {
-  const sec = Math.floor(second % 60);
-  console.log({ sec });
-  timeH.innerText = `${sec < 10 ? "0" : " "}${sec}`;
-}
-function endTime() {
-  timeH.innerText = timeSecond;
-}
-const countDown = setInterval(() => {
-  timeSecond--;
-  displayTime(timeSecond);
-  if (timeSecond <= 0 || timeSecond < 1) {
-    endTime();
-    clearInterval(countDown);
+//this function returns the initial value of time second
+function timingTimer() {
+  const resetTimer = function () {
+    return (timeSecond = 10);
+  };
+
+  // const button = document.querySelector(".btn");
+
+  function displayTime(second) {
+    //  const min = Math.floor(second / 60);
+    let sec = Math.floor(second % 60);
+    console.log({ sec });
+    timeH.innerText = /*`${min<10 ? "0": " "}${min}:*/ `${
+      sec < 10 ? "0" : " "
+    }${sec}`;
   }
-}, 1000);
-//this function starts the countdown
-const timeStart = function () {
-  countDown();
-};
+  // console.log(displayTime(timeSecond));
+  //displayTime(second)
+
+  function endTime() {
+    timeH.innerText = timeSecond;
+  }
+
+  const countDown = setInterval(() => {
+    timeSecond--;
+    displayTime(timeSecond);
+    if (timeSecond <= 0 || timeSecond < 1) {
+      endTime();
+      clearInterval(countDown);
+    }
+  }, 1000);
+  //this function starts the countdown
+  const timeStart = function () {
+    //displayTime(timeSecond);
+    countDown;
+
+    //endTime();
+  };
+
+  timeStart();
+  // countDown();
+  endTime();
+  // timeStart();
+  resetTimer();
+  displayTime(timeSecond);
+}
+
 //CLOCK CODE
 const semicircles = document.querySelectorAll(".semiCircle");
-function secondPart() {
-  //   console.log(semicircles);
+function wholeClock() {
+  // console.log(semicircles);
   let hr = 0;
   let min = 0;
-  let sec = 5;
+  let sec = timeSecond;
   let hours = hr * 3600000;
   let minutes = min * 60000;
   let seconds = sec * 1000;
   let setTime = hours + minutes + seconds;
-}
-let timerLoop = setInterval(countDownTimer);
+  const startTime = Date.now();
+  const futureTime = startTime + setTime;
 
-function clockPart1() {
-  let startTime = Date.now();
-  let futureTime = startTime + setTime;
-  return futureTime;
-  if (remainingTime < 0) {
+  // console.log(futureTime);
+  // countDownTimer();
+  const timerLoop = setInterval(countDownTimer);
+  function countDownTimer() {
+    const currentTime = Date.now();
+    const remainingTime = futureTime - currentTime;
+    const angle = (remainingTime / setTime) * 360;
+
+    if (angle > 180) {
+      semicircles[2].style.display = "none";
+      semicircles[0].style.display = "block";
+      semicircles[0].style.transform = "rotate(180deg)";
+      semicircles[1].style.transform = `rotate(${angle}deg)`;
+    } else {
+      semicircles[2].style.display = "block";
+      semicircles[0].style.transform = `rotate(${angle}deg)`;
+      semicircles[1].style.transform = `rotate(${angle}deg)`;
+    }
+
+    if (remainingTime < 0) {
+      clearInterval(timerLoop);
+    }
+  }
+  countDownTimer();
+  const currentTime = Date.now();
+  const remainingTime = futureTime - currentTime;
+
+  if (remainingTime === timeSecond) {
     clearInterval(timerLoop);
-    // timeH.innerText = `0`;
   }
 }
-let futureTime = clockPart1();
-//countDownTimer();
-function countDownTimer() {
-  let currentTime = Date.now();
-  let remainingTime = futureTime - currentTime;
-  let angle = (remainingTime / setTime) * 360;
-  if (angle > 180) {
-    semicircles[2].style.display = "none";
-    // semicircles[0].style.display = "block";
-    semicircles[0].style.transform = "rotate(180deg)";
-    semicircles[1].style.transform = `rotate(${angle}deg)`;
-  } else {
-    semicircles[2].style.display = "block";
-    semicircles[0].style.transform = `rotate(${angle}deg)`;
-    semicircles[1].style.transform = `rotate(${angle}deg)`;
-  }
-  //timer
-  //   const secs = Math.floor((remainingTime / 1000) % 60);
-  //   timeH.innerText = `${secs}`;
-  if (remainingTime < 0) {
-    clearInterval(timerLoop);
-  }
-}
-// console.log(nextButton);
+// wholeClock();
 
-// nextButton = document.querySelector("#nextButton");
+btn.addEventListener("click", wholeClock);
+btn.addEventListener("mousedown", timingTimer);
 
-// nextButton.addEventListener("click", displayTime);
-// nextButton.addEventListener("click", endTime);
-// nextButton.addEventListener("click", countDownTimer);
-nextButton.addEventListener("click", countDownTimer);
-nextButton.addEventListener("click", futureTime);
+// btn.addEventListener("click", countDownTimer);
